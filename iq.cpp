7546cdd9c81f30073ba8c12081c0fca9b90ca14e
Ayush Kumar <80770455+trivedi-ayush@users.noreply.github.com>
2022-03-27 20:49:30 +0530

    Add files via upload

---
+++ iq.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+    int x;
+    cin>>x;
+    if(x+7>170)
+    {
+        cout<<"YES"<<"\n";
+    }
+    else
+    {
+        cout<<"NO"<<"\n";
+    }
+    
+}

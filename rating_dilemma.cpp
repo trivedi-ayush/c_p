11b1a415f4cac58789b46707c2772a9ce40baa77
Ayush Kumar <80770455+trivedi-ayush@users.noreply.github.com>
2022-03-27 20:49:57 +0530

    Add files via upload

---
+++ rating_dilemma.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    long int t;
+    cin>>t;
+    while(t--)
+    {
+        int s;
+        cin>>s;
+        int y=s-(-1);
+        y=y*(-1);
+        cout<<y<<"\n";
+    }
+
+}

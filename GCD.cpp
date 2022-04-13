b1b168cdf4968c9cdf45220d7b2abc6301805156
Ayush Kumar <80770455+trivedi-ayush@users.noreply.github.com>
2022-04-13 21:10:00 +0530

    Add files via upload

---
+++ GCD.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b;
+        cin>>a>>b;
+        if(a%2==0)
+        {
+            if(b-a>=2)
+            {
+                cout<<a<<" "<<a+2<<endl;
+            }
+            else{
+                cout<<-1<<endl;
+            }
+        }
+        else
+        {
+            if(b-a>=3)
+            {
+                if(a%3==0)
+                {
+                    cout<<a<<" "<<a+3<<endl;
+                }
+                else
+                {
+                    cout<<a+1<<" "<<a+3<<endl;
+                }
+            }
+            else
+            {
+                cout<<-1<<endl;
+            }
+        }
+    }
+    return 0;
+}
